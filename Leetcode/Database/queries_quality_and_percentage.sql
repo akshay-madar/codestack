06977e70d78e0c1e693e7e79c070acba8c861c8f
akshay-madar <akshay-madar@users.noreply.github.com>
2020-02-10 16:52:55 -0500

    Create queries_quality_and_percentage.sql

---
+++ Leetcode/Database/queries_quality_and_percentage.sql
@@ -0,0 +1,5 @@
+select query_name,
+        round(avg(rating/position), 2) quality,
+        round(sum(case when rating < 3 then 1 else 0 end)/count(rating) * 100,2) poor_query_percentage
+from queries
+group by query_name;
