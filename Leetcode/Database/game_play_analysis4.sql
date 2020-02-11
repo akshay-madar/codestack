362d32c0334df800d09e31f8fd7b9f073a32d829
akshay-madar <akshay-madar@users.noreply.github.com>
2020-02-10 20:24:32 -0500

    Create game_play_analysis4.sql

---
+++ Leetcode/Database/game_play_analysis4.sql
@@ -0,0 +1,6 @@
+select round(sum(case when t1.event_date = t2.first_event+1 then 1 else 0 end)/count(distinct t1.player_id), 2) as fraction
+from Activity as t1 inner join
+    (select player_id, min(event_date) as first_event
+    from Activity
+    group by player_id) as t2
+on t1.player_id = t2.player_id;
