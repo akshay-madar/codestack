3a1661d173e4dd6aba5d75264688ec5a021351be
akshay-madar <akshay-madar@users.noreply.github.com>
2020-02-10 20:34:36 -0500

    Update winning_candidate.sql

--- Leetcode/Database/winning_candidate.sql
+++ Leetcode/Database/winning_candidate.sql
@@ -1,5 +1,5 @@
 select candidate.name
 from candidate, vote
-where candidate.id = CandidateId
+where candidate.id = candidateid
 group by name
-having count(candidate.id) = (select max(count(candidateId)) from vote group by candidateid)
+having count(candidate.id) = (select max(count(candidateid)) from vote group by candidateid)
