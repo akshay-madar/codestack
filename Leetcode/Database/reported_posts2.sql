13a4752f5f4802b617c575137a577f3cf5c5f0d8
akshay-madar <akshay-madar@users.noreply.github.com>
2020-02-14 00:43:42 -0500

    Create reported_posts2.sql

---
+++ Leetcode/Database/reported_posts2.sql
@@ -0,0 +1,12 @@
+
+select round(avg(removed_post/total_post) * 100, 2) as average_daily_percent
+from
+    (
+    select action_date,
+           count(distinct r.post_id) as removed_post,
+           count(distinct a.post_id) as total_post
+    from actions a, removals r
+    where a.post_id = r.post_id(+)
+    and action = 'report' and extra = 'spam'
+    group by action_date
+    ) tem
