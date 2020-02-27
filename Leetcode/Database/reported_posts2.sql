52e993a476d413e249b5efbd1cb5c33dcf3cc9ba
akshay-madar <akshay-madar@users.noreply.github.com>
2020-02-27 18:41:20 -0500

    Update reported_posts2.sql

--- Leetcode/Database/reported_posts2.sql
+++ Leetcode/Database/reported_posts2.sql
@@ -10,3 +10,10 @@ from
     and action = 'report' and extra = 'spam'
     group by action_date
     ) tem
+
+--another solution
+    select round(avg(a.rate) *100,2) as average_daily_percent from
+    (select action_date, count(distinct r.post_id)/ count(distinct a.post_id) as rate
+    from actions a, removals r where a.post_id = r.post_id(+)
+    and action = 'report' and extra = 'spam'
+    group by action_date) a
