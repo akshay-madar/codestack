f489e9613d2a5e5580c315f6282b269f1f07fdbd
akshay-madar <akshay-madar@users.noreply.github.com>
2020-04-10 13:53:44 -0400

    Create occupations.sql

---
+++ HackerRank/SQL/occupations.sql
@@ -0,0 +1,10 @@
+select d,p,s,a from
+(
+  select Name,Occupation,(row_number() over(partition by Occupation order by Name)) as rn
+  from Occupations
+)
+pivot
+(
+  max(Name) for Occupation in ('Doctor' as d,'Professor' as p,'Singer' as s,'Actor' as a)
+)
+order by rn;
