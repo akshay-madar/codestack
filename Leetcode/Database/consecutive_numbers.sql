91a8dc3b11c970944119e2d9456a2264cd06ea10
akshay-madar <akshay-madar@users.noreply.github.com>
2020-04-15 21:17:20 -0400

    Update consecutive_numbers.sql

--- Leetcode/Database/consecutive_numbers.sql
+++ Leetcode/Database/consecutive_numbers.sql
@@ -2,3 +2,11 @@ select distinct a.num as consecutivenums
 from logs a, logs b, logs c
 where a.num = b.num and b.num = c.num
 and a.id = b.id - 1 and b.id = c.id - 1
+
+SELECT DISTINCT num
+FROM
+(
+SELECT num,LEAD(num) OVER(ORDER BY id) AS lead, LAG(num) OVER (ORDER BY id) AS lag
+FROM logs
+) as t
+WHERE num=lead and num=lag;
