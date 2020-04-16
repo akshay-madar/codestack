b259076901355d720a1393f7f1dc769e11400504
akshay-madar <akshay-madar@users.noreply.github.com>
2020-04-15 21:17:29 -0400

    Update sales_analysis2.sql

--- Leetcode/Database/sales_analysis2.sql
+++ Leetcode/Database/sales_analysis2.sql
@@ -7,3 +7,9 @@ where Sales.product_id = Product.product_id
     from Sales inner join Product
     where Sales.product_id = Product.product_id
         and product_name = 'iPhone')
+
+
+select distinct buyer_id
+from sales, product
+where buyer_id not in (select buyer_id from sales, product where product.product_id = sales.product_id and product_name = 'iPhone')
+and product.product_id = sales.product_id and product_name = 'S8' ;
