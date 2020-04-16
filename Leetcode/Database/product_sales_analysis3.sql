ce60f4c0ad487d667c233fa8b50dbc87c5d32bf9
akshay-madar <akshay-madar@users.noreply.github.com>
2020-04-16 19:52:27 -0400

    Update product_sales_analysis3.sql

--- Leetcode/Database/product_sales_analysis3.sql
+++ Leetcode/Database/product_sales_analysis3.sql
@@ -2,3 +2,10 @@ select sales.product_id, sales.year first_year, quantity, price
 from sales, (select product_id, min(year) year
             from sales group by product_id) tmp
 where sales.year = tmp.year and sales.product_id = tmp.product_id
+
+
+
+/* Write your PL/SQL query statement below */
+select product_id, year as first_year, quantity, price
+from sales
+where (product_id, year) in (select product_id, min(year) from sales group by product_id);
