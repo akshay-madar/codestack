65ef72578a279593196be6ed36c35806651ae579
akshay-madar <akshay-madar@users.noreply.github.com>
2020-04-16 19:52:23 -0400

    Update find_the_team_size.sql

--- Leetcode/Database/find_the_team_size.sql
+++ Leetcode/Database/find_the_team_size.sql
@@ -2,3 +2,11 @@ select a.employee_id, b.count as team_size
 from employee a join (select team_id, count(*) as count
 from employee group by team_id) b on
 a.team_id = b.team_id
+
+
+select employee_id, a.team_size
+from employee, (
+select team_id, count(team_id) team_size
+from employee
+group by team_id) a
+where employee.team_id = a.team_id;
