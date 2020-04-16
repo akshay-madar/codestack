ddc178345b0ae981b8e49ab77bb4008601b8de67
akshay-madar <akshay-madar@users.noreply.github.com>
2020-04-16 19:52:10 -0400

    Update average_selling_price.sql

--- Leetcode/Database/average_selling_price.sql
+++ Leetcode/Database/average_selling_price.sql
@@ -4,3 +4,10 @@ from unitssold a, prices b
 where a.product_id = b.product_id and
 a.purchase_date between b.start_date and b.end_date
 group by a.product_id;
+
+select unitssold.product_id,
+round(sum(units*price)/sum(units), 2) average_price
+from unitssold, prices
+where prices.product_id = unitssold.product_id
+and purchase_date between start_date and end_date
+group by unitssold.product_id;
